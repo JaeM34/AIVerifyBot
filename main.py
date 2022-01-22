029d38ca49620d4af93c253bb2f2ec0c82a89d03
Danielkim002 <danielskim02dsk@gmail.com>
2022-01-22 12:47:05 -0800

    Outline for bot steps prepared and noted

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ import discord
 botToken = ""
 botUser = "Verify"
 
-print("Hello world")
 
-print("Goodbye world")
+#first we will have the bot check for everyone with a @everyone role
+#verified users will have member role
+#dm users with @everyone role
 
-def good_method():
-    print("This is a good method")
-    #test
-    #test commit
-    #test commit 2
-    #testing sync
+#bot will say "please enter your csun email"
+for i in range(0,3):
+    email = "" 
+    #bot check if email contains @my.csun.edu
+    def check_Email(str):
+        if "@my.csun.edu" in str:
+            return True
+        return False
+    if check_Email(email):
+        #give user the role of Member within the discord
+        print("hurray!")
+        break
+        None
+    else:
+        #bot will say "This email is invalid, please check and make sure the email you sent was correct"
+        print("booo!")
+        None
+if i == 2:
+    print("please contact one of the club officers to have this issue resolved")
+
